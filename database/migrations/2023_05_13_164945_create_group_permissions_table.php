cc6fa772d7f12d9b1576e67b2e2158ce99e2f242
SuradaKM <surada.k@firecreekweb.com>
2023-05-14 08:08:20 +0700

    migrate db

--- database/migrations/2023_05_13_164945_create_group_permissions_table.php
+++ database/migrations/2023_05_13_164945_create_group_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateGroupPermissionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
+        Schema::create('group_permissions', function (Blueprint $table) {
             $table->id();
-            $table->integer('user_id')->index();
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->integer('group_id')->index();
+            $table->integer('permission_id')->index();
+            $table->tinyInteger('is_enable');
         });
     }
 
@@ -29,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('group_permissions');
     }
 }
