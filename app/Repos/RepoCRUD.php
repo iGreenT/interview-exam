7fda3becb60c0473585f7538c0d6a50c3b46ee0b
SuradaKM <surada.k@firecreekweb.com>
2023-05-14 12:32:24 +0700

    setup traits CRUD

--- app/Repos/RepoCRUD.php
+++ app/Repos/RepoCRUD.php
@@ -2,6 +2,8 @@
 
 namespace App\Repos;
 
+use Illuminate\Database\Eloquent\Model;
+
  trait RepoCRUD
  {    
     /**
@@ -11,12 +13,61 @@ namespace App\Repos;
      * @param  bool $strict
      * @return void
      */
-    public function findById(int $id, bool $strict = true)
+    public function findById(int $id, bool $strict = true): Model
     {
         if ($strict) {
             return $this->model->findOrFail($id);
         }
 
         return $this->model->where('id', $id)->first();
+    }
+    
+    /**
+     * Create record
+     *
+     * @param  array $data
+     * @return Model
+     */
+    public function create(array $data): Model
+    {
+        return $this->model->create($data);
+    }
+    
+    /**
+     * Update by record id
+     *
+     * @param  int   $id
+     * @param  array $data
+     * @return Model
+     */
+    public function updateById(int $id, array $data): Model
+    {
+        $record = $this->model->findOrFail($id);
+        $record->update($data);
+
+        return $record;
+    }
+    
+    /**
+     * Update data at many row (by ids) in one time
+     *
+     * @param  array<int> $ids
+     * @param  array      $data
+     * @return void
+     */
+    public function updateByIds(array $ids, array $data): void
+    {
+        $this->model->whereIn('id', $ids)->update($data);
+    }
+    
+    /**
+     * Deleted by record id
+     *
+     * @param  int $id
+     * @return bool
+     */
+    public function deletedById(int $id): bool
+    {
+        return $this->model->findOrFail($id)->delete();
     }
  }
